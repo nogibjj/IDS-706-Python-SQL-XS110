3d5d27523b38ac2682992f98a043ffd88c20c768
Xinyi Sheng <111578487+xs110@users.noreply.github.com>
2023-10-03 03:50:59 +0000

    test

--- test_main.py
+++ test_main.py
@@ -1,20 +1,66 @@
-import main  # Import your main module
-import polars as pl
+import unittest
+from unittest.mock import patch
+import main
 
 
-def test_main():
-    """Test main.py"""
-    input_file_path = "world_population.csv"  # Update with your dataset file path
-    summary_report = "report.md"
-    histogram_image = "image/population_histogram.png"
-    boxplot_image = "image/population_boxplot.png"
+class TestDatabaseConnection(unittest.TestCase):
+    @patch("builtins.print")  # Mock the print function to capture output
+    def test_connection(self, mock_print):
 
-    # Read the dataset using pandas
-    data = pl.read_csv(input_file_path)
+        # Call the connect_to_database function
+        main.connect_to_database()
 
-    # Call the function from main.py with the DataFrame
-    main.save_report(data, summary_report, histogram_image, boxplot_image)
+        # Assertions based on the expected output
+        mock_print.assert_any_call("Connected to SQLite database")
+        mock_print.assert_any_call("Connection closed")
+
+
+class TestCreateOperation(unittest.TestCase):
+    @patch("builtins.print")  # Mock the print function to capture output
+    def test_create_operation(self, mock_print):
+        # Call the create_operation function
+        main.create_operation()
+
+        # Assertions based on the expected output
+        mock_print.assert_any_call("Table created successfully!")
+        mock_print.assert_any_call("Connection closed")
+
+
+class TestReadoperation(unittest.TestCase):
+    @patch("builtins.print")  # Mock the print function to capture output
+    def test_read_operation(self, mock_print):
+
+        # Call the connect_to_database function
+        main.read_operation()
+
+        # Assertions based on the expected output
+        mock_print.assert_any_call("Data in the table:")
+        mock_print.assert_any_call("Connection closed")
+
+
+class TestUpdateoperation(unittest.TestCase):
+    @patch("builtins.print")  # Mock the print function to capture output
+    def test_update_operation(self, mock_print):
+
+        # Call the connect_to_database function
+        main.update_operation()
+
+        # Assertions based on the expected output
+        mock_print.assert_any_call("Record updated successfully!")
+        mock_print.assert_any_call("Connection closed")
+
+
+class TestDeleteoperation(unittest.TestCase):
+    @patch("builtins.print")  # Mock the print function to capture output
+    def test_delete_operation(self, mock_print):
+
+        # Call the connect_to_database function
+        main.delete_operation()
+
+        # Assertions based on the expected output
+        mock_print.assert_any_call("Record deleted successfully!")
+        mock_print.assert_any_call("Connection closed")
 
 
 if __name__ == "__main__":
-    test_main()
+    unittest.main()
